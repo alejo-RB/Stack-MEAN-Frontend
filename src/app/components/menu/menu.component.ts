a3e4131b631a376458a3de8979b2991cfa96908b
alejo-RB <rodrgueza@yahoo.es>
2023-05-22 03:13:40 -0500

    update

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -13,4 +13,24 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  changeColor(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    target.style.backgroundColor = '#c49b28'; // Cambia el color según tus preferencias
+  }
+
+  resetColor(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    target.style.backgroundColor = '#FFC72C'; // Restablece el color original
+  }
+
+  changeColorSalir(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    target.style.backgroundColor = '#ac231a'; // Cambia el color según tus preferencias
+  }
+
+  resetColorSalir(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    target.style.backgroundColor = '#DA291C'; // Restablece el color original
+  }
+
 }
