274705742a1127ef8a3ce0b635de178490d59c9f
alejo-RB <rodrgueza@yahoo.es>
2023-05-24 01:10:19 -0500

    update

--- src/app/components/portada/portada.component.ts
+++ src/app/components/portada/portada.component.ts
@@ -9,6 +9,14 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 export class PortadaComponent implements OnInit {
   imagenes: any[] = [{ name: 'sanduche', img: './assets/img/carrusel1.jpg' }, {    name: "hamburguesa", img: "./assets/img/carrusel2.png"}, { name: "vendedora", img: "./assets/img/carrusel3.jpg", description: "Nos cuidamos entre todos"}];
 
+  constructor(config: NgbCarouselConfig) {
+		// customize default values of carousels used by this component tree
+		config.interval = 4000;
+		config.wrap = true;
+		config.keyboard = false;
+		config.pauseOnHover = true;
+	}
+
   changeColor(event: MouseEvent) {
     const target = event.target as HTMLElement;
     target.style.backgroundColor = '#c49b28'; // Cambia el color según tus preferencias
@@ -19,7 +27,6 @@ export class PortadaComponent implements OnInit {
     target.style.backgroundColor = '#FFC72C'; // Restablece el color original
   }
 
-  constructor(private _config: NgbCarouselConfig) {}
 
   ngOnInit(): void {}
 }
